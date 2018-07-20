fa19204247b2c9653315d9390e68afe139c3aca3
feysal-Ibrahim <addictivefazman@gmail.com>
2018-07-20 17:09:48 +0300

    add more statement on javaScript file

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,15 @@ function Player(turn) {
   this.turn = turn;
   this.playerName;
 }
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.tempscore = 0;
+  alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
+  // this.changeturn();
+  } else {
+  this.tempscore += this.roll;
+  }
+}
 
 
 
