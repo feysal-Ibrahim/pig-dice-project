3e3a7991f036c0185a44bb7563ad738b57cd6665
feysal-Ibrahim <addictivefazman@gmail.com>
2018-07-20 16:56:20 +0300

    added j query effect

--- js/scripts.js
+++ js/scripts.js
@@ -28,21 +28,21 @@ $(document).ready(function() {
 
       $(".start-menu").show();
     });
-   $("button#player1-roll").click(function(event){
+   $("button#player1roll").click(function(event){
       player1.roll = throwdice();
       $("#die-roll-1").text(player1.roll);
       player1.rollone();
       $("#round-total-1").text(player1.tempscore);
     });
 
-    $("button#player2-roll").click(function(event){
+    $("button#player2roll").click(function(event){
       player2.roll = throwdice();
       $("#die-roll-2").text(player2.roll);
       player2.rollone();
       $("#round-total-2").text(player2.tempscore);
     });
 
-    $("button#player1-hold").click(function(event){
+    $("button#player1hold").click(function(event){
       player1.hold();
       $("#total-score-1").text(player1.totalscore);
       $("#round-total-1").empty();
@@ -50,7 +50,7 @@ $(document).ready(function() {
       player1.winnerCheck();
     });
 
-    $("button#player2-hold").click(function(event){
+    $("button#player2hold").click(function(event){
       player2.hold();
       $("#total-score-2").text(player2.totalscore);
       $("#round-total-2").empty();
