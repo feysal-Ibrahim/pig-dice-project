91bf91c8d27518372c4fc298e44f0a81a332cd46
feysal-Ibrahim <addictivefazman@gmail.com>
2018-07-20 12:44:15 +0300

    add some alert message in the javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
-$(document).ready function() {
-	$("button#start").click function(event) {
-		player1 = new player (true);
-		player2 = new palyer (false);
+$(document).ready(function() {
 
+  $("button#start").click(function(event){
+    player1 = new Player(true);
+    player2 =  new Player(false);
+    $(".player-console").show();
 
+    var player1Name = $(".player1Name").val();
+    $("#player1Name").text(player1Name);
 
+    var player2Name = $(".player2Name").val();
+    $("#player2Name").text(player2Name);
+});
+  $("button#new-game").click(function(event) {
+    $(".player-console").hidd();
+    clearValues();
+    player1.newGame();
+    player2.newGame();
+    $("#round-total-1").empty();
+      $("#total-score-1").empty();
+      $("#roll-1").empty();
+      $("#round-total-2").empty();
+      $("#total-score-2").empty();
+      $("#roll-2").empty();
 
-alert("have a nice game!!PRESS OK TO CONTINUE ")
+      $(".start-menu").show();
+    });
